f23163a203d6f2eb5c8efa0c7c263180d5df4c8f
AymnBnShaban <aymnm1957@gmail.com>
2025-05-14 23:46:09 +0300

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,17 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: const HomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: const HomeView(),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
