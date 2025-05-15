58655be6ba8a3ac06244689684cfef7e03eb3bb2
AymnBnShaban <aymnm1957@gmail.com>
2025-05-15 21:12:19 +0300

    small change

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.indigo[600]!.withOpacity(0.179),
+      body: NotesViewBody(),
+    );
   }
 }
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        SizedBox(
+          height: 100,
+        ),
+        Text('Notes',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24
+        ),
+        ),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
