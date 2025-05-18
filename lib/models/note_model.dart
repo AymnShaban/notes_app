f28b66620640e40f8421b9d028c448949610e1b4
AymnBnShaban <aymnm1957@gmail.com>
2025-05-18 09:52:36 +0300

    automatically generating Hive Model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,20 @@
+/* after writing @HiveType(typeId: 0) and  @HiveField(2) to the model
+   we use a command to automatically generate
+  " flutter packages pub run build_runner build "
+*/
+
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
 class NoteModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
