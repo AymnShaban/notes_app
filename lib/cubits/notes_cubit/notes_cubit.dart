66dc94ab3ad46ffdfd94024dd9558fe72455effc
AymnBnShaban <aymnm1957@gmail.com>
2025-05-21 22:58:55 +0300

    create fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,11 +1,24 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:notes_app/models/note_model.dart';
+
+import '../../constant.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+  fetchAllNotes() async {
+    emit(NotesLoadingState());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNoteBox);
+
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccessState(notes));
+    } on Exception catch (e) {
+      emit(NotesFailureState(e.toString()));
+    }
+  }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 import '../widgets/custom_floating_action_button.dart';
 
@@ -7,11 +9,14 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // 0xff
-      floatingActionButton: const CustomFloatingActionButton(),
-      backgroundColor: Colors.indigo[600]!.withValues(alpha: 0.179),
-      body: const NotesViewBody(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        // 0xff
+        floatingActionButton: const CustomFloatingActionButton(),
+        backgroundColor: Colors.indigo[600]!.withValues(alpha: 0.179),
+        body: const NotesViewBody(),
+      ),
     );
   }
 }
